b6f63027ed29497e8d4b237adb8c327816efb89b
Ted Robinson <tfqbtd@aol.com>
2018-05-23 23:37:30 -0500

    added time and db

--- index.js
+++ index.js
@@ -29,4 +29,23 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => {
+    let result = '';
+    const times = process.env.Times || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' ';
+    }
+    res.send(result);
+  })
+  .get('/db', async (req, res) => {
+    try {
+      const client = await SVGPolygonElement.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      cleint.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
