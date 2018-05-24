e4c65f32e35ffcdfc60955fb504f491b1ce74f1a
Ted Robinson <tfqbtd@aol.com>
2018-05-23 23:17:09 -0500

    Add cool face API

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema, table_name FROMinformation_schema.tables;',
+  (err, res)  =>{
+    if (err) { 
+      console.log(err);
+      throw err;
+    }
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
